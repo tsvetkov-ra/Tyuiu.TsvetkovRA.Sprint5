43e4412517ace22bc95388944e0254f12a224d5b
Admin <artykleshine@gmail.com>
2025-12-10 23:42:11 +0300

    Task4

---
+++ Tyuiu.TsvetkovRA.Sprint5.Task4.V27.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.TsvetkovRA.Sprint5.Task4.V27.Lib
+{
+    public class DataService : ISprint5Task4V27
+    {
+        public double LoadFromDataFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string strX = File.ReadAllText(path);
+                string z = strX.Replace('.', ',');
+                double x = (Convert.ToDouble(z));
+                double y = (Math.Pow(x, 3) - 4 * x) / Math.Cos(x);
+                return Math.Round(y);
+            }
+        }
+    }
+}
---
+++ Tyuiu.TsvetkovRA.Sprint5.Task4.V27/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TsvetkovRA.Sprint5.Task4.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService DataService = new DataService();
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                              *");
+        Console.WriteLine("****************************************************************");
+        string path = $@"C:\DataSprint5\InPutDataFileTask4V27.txt";
+        Console.WriteLine("Путь к файлу: " + path);
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine("*РЕЗУЛЬТАТ:                                                     ");
+        Console.WriteLine("****************************************************************");
+        double y = DataService.LoadFromDataFile(path);
+        Console.WriteLine(y);
+        Console.ReadKey();
+    }
+}
